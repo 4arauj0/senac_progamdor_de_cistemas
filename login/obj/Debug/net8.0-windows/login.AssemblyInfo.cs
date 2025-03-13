2eb4b136d08df1689cd5aab730c808fe0f059f2a
FELIPE ARAUJO DE OLIVEIRA <felipe.aoliveira54@senacsp.edu.br>
2025-03-12 22:06:34 -0300

    projeto

--- login/obj/Debug/net8.0-windows/login.AssemblyInfo.cs
+++ login/obj/Debug/net8.0-windows/login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+716708e8f34968fce14679ab0e792ee535eda705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0295d32a433f21be10f3537687eabf52bf9bc9ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
